6d49f47dd47f19ebcf3e9151ab0dbfd04dc9044b
Boev Dmitriy <Dmitr-by@mail.ru>
2021-01-22 15:17:15 +0300

    update Sep scripts

--- Remoute_psexec_SEP_Updater.ps1
+++ Remoute_psexec_SEP_Updater.ps1
@@ -2,7 +2,7 @@
 #[Console]::outputEncoding = [System.Text.Encoding]::GetEncoding('cp866')
 
 # Чтение данных из файла
-$pc = Get-Content -Path "C:\LOGS\PcForSepInstall.txt"
+$pc = Get-Content -Path "C:\LOGS\AllPC.txt"
 
 # Проверка пк на онлайн и запуск команды
 foreach ($computer in $pc){
@@ -10,12 +10,11 @@ foreach ($computer in $pc){
    if (test-Connection -Cn $computer -quiet) {
    write-host "$computer is online" -BackgroundColor DarkGreen
 
-   # запуск удалённой команды Учтановки SEP
-    c:\PsExec.exe -d -s \\$computer "\\uran.hq.icfed.com\SEP & Sylink Files\Install\setup_x64.exe" cmd /S 
-    
+# запуск удалённой команды 
+    c:\PsExec.exe -d -s \\$computer "C:\Program Files (x86)\Symantec\Symantec Endpoint Protection\SepLiveUpdate.exe" cmd /S 
+    #c:\PsExec.exe -d -s \\$computer "C:\Program Files\Symantec\Symantec Endpoint Protection\SepLiveUpdate.exe" cmd /S 
    } 
    else {
   
    }
 }
-
