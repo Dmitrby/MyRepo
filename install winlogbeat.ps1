962e4a3176023910662b8c02cbd4edddcb2f1edb
Boev Dmitriy <Ditr-y@mail.ru>
2021-04-01 15:33:22 +0300

    add files

---
+++ install winlogbeat.ps1
@@ -0,0 +1,11 @@
+﻿$servername ="EXCH01.hq.icfed.com"
+cp -Recurse "C:\Users\usertemp\Desktop\winlogbeat"    -Destination "\\$servername\c$\Program Files"
+
+
+Enter-PSSession $servername
+Invoke-Command -ComputerName $servername -ScriptBlock {
+powershell -NoProfile -ExecutionPolicy Bypass -File "C:\Program Files\winlogbeat\install-service-winlogbeat.ps1"
+Start-Service -name winlogbeat
+Get-Service -Name winlogbeat
+}
+Exit-PSSession
