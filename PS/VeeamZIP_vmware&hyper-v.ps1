2e96a2133471f15d472785188dfc0a19e1e1494e
Боев Дмитрий Юрьевич <boev@hq.icfed.com>
2021-08-05 11:33:41 +0300

    ++
    ++

--- PS/VeeamZIP_vmware&hyper-v.ps1
+++ PS/VeeamZIP_vmware&hyper-v.ps1
@@ -8,10 +8,10 @@ $Error.Clear()
 ##################################################################
 
 # Names of VMs to backup separated by comma (Mandatory). For instance, $VMNames = “VM1”,”VM2”
-$VMNames = ''
+$VMNames = 'машин'
 
 # Name of vCenter or standalone host VMs to backup reside on (Mandatory)
-$HostName = ''
+$HostName = 'сервер'
 
 # Directory that VM backups should go to (Mandatory; for instance, C:\Backup)
 $Directory = "\\***\$VMNames"
@@ -43,13 +43,13 @@ $Type = "Hyper-v"
 $EnableNotification = $True
 
 # Email SMTP server
-$SMTPServer = ""
+$SMTPServer = "смтп"
 
 # Email FROM
 $EmailFrom = "VeeamZIP@" 
 
 # Email TO
-$EmailTo = ""
+$EmailTo = "кому"
 
 # Email subject
 $EmailSubject = "VeeamZIP $VMNames"
@@ -74,7 +74,6 @@ Asnp VeeamPSSnapin
 $Server = Get-VBRServer -name $HostName
 $MesssagyBody = @()
 
-
 foreach ($VMName in $VMNames)
 {
 
@@ -97,32 +96,44 @@ foreach ($VMName in $VMNames)
   {
     $TaskSessions = $ZIPSession.GetTaskSessions().logger.getlog().updatedrecords
     $FailedSessions =  $TaskSessions | where {$_.status -eq "EWarning" -or $_.Status -eq "EFailed"}
+
   
   if ($FailedSessions -ne $Null)
   {
+	$MesssagyBody = ("$VMNames $HostName Backup_NOT_Created").trim() | ConvertFrom-String -PropertyNames VM,HOST,ERROR
     $MesssagyBody = $MesssagyBody + ($ZIPSession | Select-Object @{n="Name";e={($_.name).Substring(0, $_.name.LastIndexOf("("))}} ,@{n="Start Time";e={$_.CreationTime}},@{n="End Time";e={$_.EndTime}},Result,@{n="Details";e={$FailedSessions.Title}})
+	
+	If ($EnableNotification)
+	{
+	$Message = New-Object System.Net.Mail.MailMessage $EmailFrom, $EmailTo
+	$Message.Subject = $EmailSubject
+	$Message.IsBodyHTML = $True
+	$message.Body = $MesssagyBody | ConvertTo-Html -head $style | Out-String
+	$SMTP = New-Object Net.Mail.SmtpClient($SMTPServer)
+	$SMTP.Send($Message)
+	}
+  
   }
    
   Else
   {
-    $MesssagyBody = $MesssagyBody + ($ZIPSession | Select-Object @{n="Name";e={($_.name).Substring(0, $_.name.LastIndexOf("("))}} ,@{n="Start Time";e={$_.CreationTime}},@{n="End Time";e={$_.EndTime}},Result,@{n="Details";e={($TaskSessions | sort creationtime -Descending | select -first 1).Title}})
-  }
-  
+
+    #$MesssagyBody = $MesssagyBody + ($ZIPSession | Select-Object @{n="Name";e={($_.name).Substring(0, $_.name.LastIndexOf("("))}} ,@{n="Start Time";e={$_.CreationTime}},@{n="End Time";e={$_.EndTime}},Result,@{n="Details";e={($TaskSessions | sort creationtime -Descending | select -first 1).Title}})
+	
   }   
 }
-
-If ($EnableNotification)
-{
-$Message = New-Object System.Net.Mail.MailMessage $EmailFrom, $EmailTo
-$Message.Subject = $EmailSubject
-$Message.IsBodyHTML = $True
-$message.Body = $MesssagyBody | ConvertTo-Html -head $style | Out-String
-$SMTP = New-Object Net.Mail.SmtpClient($SMTPServer)
-$SMTP.Send($Message)
 }
 
 	If ($error.count -gt 0) 
 	{
+	
+	$Message = New-Object System.Net.Mail.MailMessage $EmailFrom, $EmailTo
+	$Message.Subject = $EmailSubject
+	$Message.IsBodyHTML = $True
+	$message.Body = (("$VMNames $HostName Backup_NOT_Created").trim() | ConvertFrom-String -PropertyNames VM,HOST,ERROR) | ConvertTo-Html -head $style | Out-String
+	$SMTP = New-Object Net.Mail.SmtpClient($SMTPServer)
+	$SMTP.Send($Message)
+		
 	exit 111
 	}
 	Else 
