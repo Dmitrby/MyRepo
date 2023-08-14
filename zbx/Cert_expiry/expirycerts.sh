06d9726b8be968c2e39614b5f430d24a7fd8c733
Dmitrby <dmitr-by@mail.ru>
2023-08-14 13:33:03 +0300

    add cert expire from dir

---
+++ zbx/Cert_expiry/expirycerts.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#variables
+dir="${1}"    # certs directory
+PARAM1="${2}" # actions
+
+if [[ $PARAM1 = "Discover" ]]  # Discovery serts names
+then
+
+CERTS=$(find $dir -name *.crt -exec basename {} \;)
+
+echo "{"
+echo '    "data":['
+FIRST=1
+
+echo $CERTS  | tr ' ' '\n '|sort  | while read line; do
+    if [ $FIRST != 0 ]; then
+        FIRST=0
+    else
+        ELEMENT="{ \"{#NAME}\":\"$DOMAIN\" },"
+        echo "        $ELEMENT"
+    fi
+    DOMAIN=$line
+done
+DOMAIN=$(echo $CERTS | tr ' ' '\n '|sort | tail -1)
+ELEMENT="{ \"{#NAME}\":\"$DOMAIN\" }"
+echo "        $ELEMENT"
+echo '    ]'
+echo "}"
+
+else     # get expiration date by name
+
+#openssl x509 -enddate -noout -in $dir/$PARAM1
+expiryDays=$(( ($(date -d "$(openssl x509 -enddate -noout -in $dir/$PARAM1 | cut -d= -f2)" '+%s') - $(date '+%s')) / 86400 ))
+echo $expiryDays
+fi
